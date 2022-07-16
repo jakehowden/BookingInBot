d188713d35c311f32697813554df914616abd531
jakehowden <jakehowden@gmail.com>
2022-07-16 22:36:54 +0100

    finalised play database method and began booked database setup

--- Commands/booked.js
+++ Commands/booked.js
@@ -1,6 +1,32 @@
-function GetBookings(database, server, user, today) {
+import {Get24HourTimeFromDate, GetFullDateFromDate } from '../Helpers/dateTime.js';
+
+module.exports = {
+    name: 'booked',
+    description: 'Gets all current bookings',
+    aliases: ['playing'],
+    usage: '!booked',
+    cooldown: 3,
+    execute(message, server) {
+
+        // Set up DB connection
+        var pool = require('../db.js');
+        var database = pool.getPool();
+        const { commands } = message.client;
+        
+        // Create booking and confirm in channel chat
+        var bookings = GetBookings(database, server, user);
+		// TODO - handle returned bookings and send a message
+    }
+};
+
+// A function to add a new booking to the database
+// Params:
+//      database - database the booking is added to
+//      server - server the booking request came from
+//      user - user the booking is for
+function GetBookings(database, server, user) {
 	return new Promise(function(resolve, reject) {
-		var sql = `SELECT slot FROM bookings WHERE server == ` + server + ' AND user == ' + user + ' AND timestamp == ' + today;
+		var sql = `SELECT slot FROM bookings WHERE server == ` + server + ' AND user == ' + user + ' AND date == ' + GetFullDateFromDate(new Date());
 
 		database.query(sql, function (error, results, fields) {
 			if (error) {
